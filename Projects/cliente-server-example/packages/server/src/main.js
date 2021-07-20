19c49d0d0caa6dfc83ee321fd962d2335fe5be15
João Paulo <joaopaulokimi@gmail.com>
2021-07-20 19:09:35 -0300

    modificando para react app

--- Projects/cliente-server-example/packages/server/src/main.js
+++ Projects/cliente-server-example/packages/server/src/main.js
@@ -1,6 +1,4 @@
 import { createServer } from 'http';
-import { readFile } from 'fs';
-import { resolve } from 'path';
 import { parse } from 'querystring';
 
 const server = createServer((request, response) => {
@@ -21,37 +19,6 @@ const server = createServer((request, response) => {
             break;
         }
 
-        case '/sign-in': {
-
-            const path = resolve(__dirname, './pages/sign-in.html');
-            readFile(path, (error, file) => {
-                if (error){
-                    response.writeHead(500, 'Can\'t process HTML file.');
-                    response.end();
-                return;
-            }
-            response.writeHead(200);
-            response.write(file);
-            response.end();
-            });
-            break;
-        }
-
-        case '/home': {
-            const path = resolve(__dirname, './pages/home.html');
-            readFile(path, (error, file) => {
-                if (error){
-                    response.writeHead(500, 'Can\'t process HTML file.');
-                    response.end();
-                return;
-            }
-            response.writeHead(200);
-            response.write(file);
-            response.end();
-            });
-            break;
-        }
-
         case '/authenticate': {
             let data = '';
 
@@ -61,9 +28,7 @@ const server = createServer((request, response) => {
 
             request.on('end', () => {
                 const params = parse(data);
-                response.writeHead(301,{
-                    Location: '/home',
-                });
+
 
                 response.write(file);
                 response.end();
